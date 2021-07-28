93bab647e49802dcd9a74cae57109198e59d6f7c
Anthony Tong <w.anthonytong@gmail.com>
2021-07-27 22:07:13 -0700

    Started files on this project

---
+++ etch-sketch.js
@@ -0,0 +1,7 @@
+const squareDivs = document.createElement('div'); //do I need to loop to make 16x16 grid???
+const DivContainer = document.querySelector('.divContainer');
+DivContainer.appendChild(squareDivs);
+
+for (i = 0; i < gridAmount; i++) {
+
+}
