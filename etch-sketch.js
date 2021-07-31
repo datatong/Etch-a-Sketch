a9df4fe552bfe4f5c4dff0f634d631acdee1c854
Anthony Tong <w.anthonytong@gmail.com>
2021-07-31 11:21:41 -0700

    Added append element to the loop

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,7 +1,10 @@
-const squareDivs = document.createElement('div'); //do I need to loop to make 16x16 grid???
+//const squareDivs = document.createElement('div'); //Why does this need to be in the loop?
 const DivContainer = document.querySelector('.divContainer');
-DivContainer.appendChild(squareDivs);
+var gridAmount = 8;
 
+//Loop to generate any number of squares per side (16x16, 32x32, 64x64)
 for (i = 0; i < gridAmount; i++) {
-
+    const squareDivs = document.createElement('div');
+    DivContainer.appendChild(squareDivs);
+    squareDivs.className = "squareDivs";
 }
