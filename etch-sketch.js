6f5d3d75adfe3634160d906aa9466345d3102ef6
Anthony Tong <w.anthonytong@gmail.com>
2021-08-08 11:52:16 -0700

    Added hover event, control panel, etc.

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,12 +1,17 @@
-//const squareDivs = document.createElement('div'); //Why does this need to be in the loop?
 const DivContainer = document.querySelector('.divContainer');
+const squareDivs = document.createElement('div');
 var gridAmount = 8;
 
-//Loop to generate any number of squares per side (16x16, 32x32, 64x64)
+//Loop to generate any number of squares per side (16x16, 32x32, 64x64).
 for (i = 0; i < gridAmount; i++) {
     for (j = 0; j < gridAmount; j++) {
-    const squareDivs = document.createElement('div');
-    DivContainer.appendChild(squareDivs);
+    const selectSquareDivs = document.querySelector('div');
+    DivContainer.appendChild(selectSquareDivs);
     squareDivs.className = "squareDivs";
     }   
 }
+
+//Changes the square color once when the mouse pointer hovers over.
+const gridHover = document.querySelectorAll('div');
+gridHover.addEventListener('mouseenter', () => {squareDivs.style.background = 'black';
+});
