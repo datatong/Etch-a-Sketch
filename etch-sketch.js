8e7a6a10cac0bc8104c40611da8ed68625622c5d
Anthony Tong <w.anthonytong@gmail.com>
2021-08-14 13:39:04 -0700

    Square divs to change color on hover.

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,17 +1,17 @@
 const DivContainer = document.querySelector('.divContainer');
-const squareDivs = document.createElement('div');
+//const squareDivs = document.createElement('div');
 var gridAmount = 8;
 
 //Loop to generate any number of squares per side (16x16, 32x32, 64x64).
 for (i = 0; i < gridAmount; i++) {
     for (j = 0; j < gridAmount; j++) {
-    const selectSquareDivs = document.querySelector('div');
-    DivContainer.appendChild(selectSquareDivs);
-    squareDivs.className = "squareDivs";
+    const squareDivs = document.createElement('div');
+    DivContainer.appendChild(squareDivs);
+    squareDivs.classList.add("squareDivs");
+    squareDivs.addEventListener('mouseenter', () => {
+        squareDivs.style.background = "#" + Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0") + 
+        Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0") + 
+        Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0");
+        });
     }   
 }
-
-//Changes the square color once when the mouse pointer hovers over.
-const gridHover = document.querySelectorAll('div');
-gridHover.addEventListener('mouseenter', () => {squareDivs.style.background = 'black';
-});
