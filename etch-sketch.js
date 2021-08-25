da12609b86edbd86d3ee995ba3f5873e042fc8f7
Anthony Tong <w.anthonytong@gmail.com>
2021-08-24 20:56:24 -0700

    Added clear button eventlistener and clear function. Turned loop into a function.

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,21 +1,25 @@
-const DivContainer = document.querySelector('.divContainer');
-//const squareDivs = document.createElement('div');
-var gridAmount = 8;
+const divContainer = document.querySelector('.divContainer');
+const clearButton = document.querySelector('.clear');
+//Moved to for loop: const squareDivs = document.createElement('div');
 
+var gridAmount = 8;
+createGrid();
 
 //Loop to generate any number of squares per side (16x16, 32x32, 64x64).
-for (i = 0; i < gridAmount; i++) {
-    for (j = 0; j < gridAmount; j++) {
-    const squareDivs = document.createElement('div');
-    DivContainer.appendChild(squareDivs);
-    squareDivs.classList.add("squareDivs");
-    //Changes squares to a random color on mouse hover
-    squareDivs.addEventListener('mouseenter', () => {
-        squareDivs.style.background = "#" + Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0") + 
-        Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0") + 
-        Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0");
-        });
-    }   
+function createGrid() {
+    for (i = 0; i < gridAmount; i++) {
+        for (j = 0; j < gridAmount; j++) {
+        const squareDivs = document.createElement('div');
+        divContainer.appendChild(squareDivs);
+        squareDivs.classList.add("squareDivs");
+        //Changes squares to a random color on mouse hover
+        squareDivs.addEventListener('mouseenter', () => {
+            squareDivs.style.background = "#" + Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0") + 
+            Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0") + 
+            Math.floor(80 + Math.random()*100).toString(16).padStart(2, "0");
+            });
+        }   
+    }
 }
 
 //Changes the grid size on slider
@@ -25,8 +29,16 @@ function updateSlider(gridAmount) {
 }
 var gridDimensions = document.getElementById("grid-dimensions");
 var sliderDisplay = document.getElementById("gridSize");
-gridDimensions.oninput = function () {
+gridDimensions.oninput = function() {
     sliderDisplay.innerHTML = this.value;
 }
 
-//To do: create eventlistener to apply the gridsize shown on slider and to reset it
+//To remove square grid colors and recreate grid
+function clearGridColor() {
+    squareDivs.removeEventListener('mouseenter', ());
+    createGrid();
+}
+
+clearButton.addEventListener('click', clearGridColor);
+
+//To do: create eventlistener to apply the gridsize shown on slider
